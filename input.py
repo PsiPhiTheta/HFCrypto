c288b54e59527bafc7047cb81d11d6d112f4a0d5
Nicolo <nic.frisiani@gmail.com>
2017-03-11 08:02:53 +0000

    Json added inputs

--- input.py
+++ input.py
@@ -5,15 +5,18 @@ import io
 import time
 import hmac, hashlib
 
-url = "https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=9999999999&period=300"
+url = ""
+
 
 response = urllib2.urlopen(url)
 data = json.loads(response.read())
 
 
 # Writing JSON data
-with open('monero.json', 'w') as f:
+with open('', 'w') as f:
     json.dump(data, f)
+
+
 
 # Reading data back
 # with open('data.json', 'r') as f:
