80d509b793ddcb39e60aa48efff46d6963c67b83
Raul Lozano Martin <raul.lozanomartin@student.manchester.ac.uk>
2017-03-11 00:03:06 +0000

    'Interface' for reading url and convert to json files.

--- network.py
+++ network.py
@@ -2,10 +2,7 @@ from sklearn.neural_network import MLPClassifier
 
 # Array X of size (n_samples, n_features): Holds training samples
 # Array y of size (n_samples): Holds the target values
-X = [[0., 0.], [1., 1.]]
-y = [0, 1]
-clf = MLPClassifier(solver='lbfgs', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1,
+clf = MLPClassifier(activation='relu', solver='adam', alpha=1e-5, hidden_layer_sizes=(5, 2), random_state=1,
                     max_iter=200)
-clf.fit(X, y)
-y = clf.predict([[2., 2.], [-1., -2.]])
-print(y)
+
+# Get input
