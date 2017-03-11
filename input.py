e865a15158c662859399f2b8fe11ec685d1c71ca
Raul Lozano Martin <raul.lozanomartin@student.manchester.ac.uk>
2017-03-11 00:09:38 +0000

    Removed useless imports.

--- input.py
+++ input.py
@@ -1,9 +1,6 @@
-import urllib
 import urllib2
 import json
-import io
-import time
-import hmac, hashlib
+
 
 url = "https://poloniex.com/public?command=returnChartData&currencyPair=BTC_XMR&start=1405699200&end=9999999999&period=300"
 
