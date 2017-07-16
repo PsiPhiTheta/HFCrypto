f3ee5927740042dd6f6b57ab6b56a02d025aee84
Raul Lozano Martin <raullm7@gmail.com>
2017-07-16 17:05:25 +0100

    Modified to print stats

--- data/main.py
+++ data/main.py
@@ -12,18 +12,26 @@ while(True):
     data.append(time.time())
 
     with open('data.txt', 'a') as outfile:
+        numberFeatures = 0
         outfile.write(str(actualTime))
         for i in returnOrderBook.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         for i in returnCurrencies.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         for i in return24hVolume.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         for i in returnLoanOrders.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         for i in returnTicker.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         for i in githubParser.returnResult():
+            numberFeatures += 1
             outfile.write(str(i) + ',')
         outfile.write('\n')
+        print("Number of features: " + str(numberFeatures) + " | Time: " + time.strftime("%Y-%m-%d %H:%M:%S", time.gmtime()))
         time.sleep(300 - (time.time() - actualTime))
